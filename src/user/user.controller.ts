81032e0cac5debd127099aa9818be5e3915864d1
Natanael Heinrick <natanaelheinrick12@gmail.com>
2025-04-29 23:21:44 -0300

    feat: implementing JWTGuard on HTTPS routes in the user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -18,23 +18,27 @@ import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 export class UserController {
   constructor(private readonly userService: UserService) {}
   
-//@UseGuards(JwtAuthGuard)
+
   @Post()
   async create(@Body() { name, email, telephone, password }) {
     const result = await this.userService.create({ name, email, telephone, password });
     return result;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll(@Query() query: any) {
     return this.userService.findAll(query);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.userService.findOne(+id);
   }
 
+
+ @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -43,6 +47,7 @@ export class UserController {
     return this.userService.update(id, { name, email, telephone, password });
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.userService.delete(+id);
