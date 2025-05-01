624d2a50e227d71ec104f43ade9ac691205919db
Danielcard99 <dc2651065@gmail.com>
2025-05-01 20:30:08 -0300

    feat(app): import ClientModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,17 @@ import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ClientModule } from './clients/clients.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule,
+  imports: [
+    PrismaModule,
+    AuthModule,
+    ClientModule,
     JwtModule.register({
       secret: `Yj3[xV0@borz-'#T3jE9l54"ySQlpA7+`,
-      signOptions: { expiresIn: '100s' }
-    })
+      signOptions: { expiresIn: '100s' },
+    }),
   ],
   controllers: [UserController],
   providers: [UserService],
