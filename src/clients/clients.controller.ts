90496117a04f5a90f82fd3fb5a19a75f48b40e5a
Danielcard99 <dc2651065@gmail.com>
2025-05-01 20:36:29 -0300

    feat(client): set up ClientController and add POST method for client creation

---
+++ src/clients/clients.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateClientDto } from './dto/create-client.dto';
+import { ClientService } from './clients.service';
+
+@UsePipes(new ValidationPipe())
+@Controller('clients')
+export class ClientController {
+  constructor(private readonly clientService: ClientService) {}
+
+  @Post()
+  create(@Body() data: CreateClientDto) {
+    return this.clientService.create(data);
+  }
+}
