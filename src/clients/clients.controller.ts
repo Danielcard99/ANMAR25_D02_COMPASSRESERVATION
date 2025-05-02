9a5ac47ab67e4467753b32b1d0bcd8df557bb370
Danielcard99 <dc2651065@gmail.com>
2025-05-02 00:42:53 -0300

    feat(client): add GET endpoint in ClientController for listing clients

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,15 +1,18 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { ClientService } from './clients.service';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { FilterClientDto } from './dto/filter-client.dto';
 
 @UsePipes(new ValidationPipe())
 @Controller('clients')
@@ -24,5 +27,10 @@ export class ClientController {
   @Patch(':id')
   async update(@Body() data: UpdateClientDto, @Param('id') id: number) {
     return this.clientService.update(id, data);
+  }
+
+  @Get()
+  async list(@Query() filters: FilterClientDto) {
+    return this.clientService.list(filters);
   }
 }
