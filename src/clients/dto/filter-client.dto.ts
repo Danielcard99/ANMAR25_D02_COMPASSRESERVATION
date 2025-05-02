fcecbaeb39e9f90f05970d3e2e46648d2d926094
Danielcard99 <dc2651065@gmail.com>
2025-05-02 16:00:48 -0300

    refactor(dto): update status import in FilterClientDto to match renamed enum in Prisma schema

--- src/clients/dto/filter-client.dto.ts
+++ src/clients/dto/filter-client.dto.ts
@@ -1,6 +1,6 @@
 import { Type } from 'class-transformer';
 import { IsEnum, IsInt, IsOptional, IsString, Min } from 'class-validator';
-import { new_table_status } from 'generated/prisma';
+import { Status } from 'generated/prisma';
 
 export class FilterClientDto {
   @IsOptional()
@@ -28,6 +28,6 @@ export class FilterClientDto {
   cpf?: string;
 
   @IsOptional()
-  @IsEnum(new_table_status)
-  status?: new_table_status;
+  @IsEnum(Status)
+  status?: Status;
 }
